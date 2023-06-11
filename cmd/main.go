dd206b4416d6d8ed06f8af3737915cc4390ecdb8
Daniel Armas <daniel.armas9706@gmail.com>
2023-06-11 15:46:42 -0400

    Implementing the initialize code for the config and sqldatabase instances in the main package.

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"github.com/daniarmas/chat/config"
+	"github.com/daniarmas/chat/pkg/sqldatabase"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	// UNIX Time is faster and smaller than most timestamps
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
+	cfg := config.NewConfig()
+
+	db, err := sqldatabase.New(cfg)
+	if err != nil {
+		log.Fatal().Msgf("Postgres Error: %v", err)
+	}
+
+	defer db.Close()
 }
