19e0ec7a41a134e6537e5cb3174d95b5c5567ebb
Daniel Armas <daniel.armas9706@gmail.com>
2023-06-11 19:08:08 -0400

    - Adding the usecases.AuthUsecase as attribute of the Resolver. - Organazing the code.

--- server.go
+++ server.go
@@ -1,28 +1,46 @@
 package main
 
 import (
-	"log"
+	logg "log"
 	"net/http"
-	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/daniarmas/chat/config"
 	"github.com/daniarmas/chat/graph"
+	"github.com/daniarmas/chat/internal/repository"
+	"github.com/daniarmas/chat/internal/usecases"
+	"github.com/daniarmas/chat/pkg/sqldatabase"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 const defaultPort = "8080"
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
+	// UNIX Time is faster and smaller than most timestamps
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	cfg := config.NewConfig()
+
+	db, err := sqldatabase.New(cfg)
+	if err != nil {
+		log.Fatal().Msgf("Postgres Error: %v", err)
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	defer db.Close()
+
+	userRepository := repository.NewUserRepository(db)
+	refreshTokenRepository := repository.NewRefreshTokenRepository(db)
+	accessTokenRepository := repository.NewAccessTokenRepository(db)
+
+	authUsecase := usecases.NewAuthUsecase(userRepository, refreshTokenRepository, accessTokenRepository, cfg)
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{AuthUsecase: authUsecase}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", cfg.GraphqlPort)
+	logg.Fatal(http.ListenAndServe(":"+cfg.GraphqlPort, nil))
 }
